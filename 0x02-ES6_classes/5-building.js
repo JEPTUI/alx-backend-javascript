7c07aeb533599d90c89d3c101f978ee4d240471f
JEPTUI <alicejeptui@gmail.com>
2023-09-29 02:48:53 +0000

    Extends a method

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
+
+export default Building;
