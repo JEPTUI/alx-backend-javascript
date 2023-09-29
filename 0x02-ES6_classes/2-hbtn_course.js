d20c8b1ae08d3245fbdd394f9780ed531875a4c3
JEPTUI <alicejeptui@gmail.com>
2023-09-29 02:17:30 +0000

    A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,44 +1,60 @@
 class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)) {
-      throw new Error('Invalid attribute types');
-    }
-
     this._name = name;
     this._length = length;
     this._students = students;
+
+    if (typeof this._name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    if (typeof this._length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+
+    if (!Array.isArray(this._students) || !this._students.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
   }
 
+  // Getter for name
   get name() {
     return this._name;
   }
 
+  // Setter for name
   set name(newName) {
     if (typeof newName !== 'string') {
-      throw new Error('Name must be a string');
+      throw new TypeError('Name must be a string');
     }
     this._name = newName;
   }
 
+  // Getter for length
   get length() {
     return this._length;
   }
 
+  // Setter for length
   set length(newLength) {
     if (typeof newLength !== 'number') {
-      throw new Error('Length must be a number');
+      throw new TypeError('Length must be a number');
     }
     this._length = newLength;
   }
 
+  // Getter for students
   get students() {
     return this._students;
   }
 
+  // Setter for students
   set students(newStudents) {
-    if (!Array.isArray(newStudents)) {
-      throw new Error('Students must be an array');
+    if (!Array.isArray(newStudents) || !newStudents.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
     }
     this._students = newStudents;
   }
 }
+
+export default HolbertonCourse;
