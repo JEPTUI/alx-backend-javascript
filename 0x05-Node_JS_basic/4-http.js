dcb7528369a16cec5e7eae02929143fd16f506ad
JEPTUI <alicejeptui@gmail.com>
2023-11-20 15:59:03 +0300

    Create a small HTTP server using Node HTTP module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+// Create a simple HTTP server
+const app = http.createServer((req, res) => {
+  // Set the response header
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Send the response body
+  res.end('Hello Holberton School!');
+});
+
+// Make the server listen on port 1245
+const PORT = 1245;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
+
+module.exports = app;
